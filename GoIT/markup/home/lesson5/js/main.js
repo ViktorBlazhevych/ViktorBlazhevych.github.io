5706f0d26d2e4a334d6dc7a106bac05631d292cf
Viktor Blazhevych <bbmedia@mail.ru>
2015-11-07 21:47:56 +0200

    lesson5

--- GoIT/markup/home/lesson5/js/main.js
+++ GoIT/markup/home/lesson5/js/main.js
@@ -8,7 +8,8 @@ function switchBG(e) {
 	var imageBG = document.getElementById('imageBG');
 	var contentHolder = document.getElementById('contentHolder');
 
-	videoStage.style.opacity = showVideo ? 1 : 0;
-	imageBG.style.opacity = showVideo ? 0 : 1;
+	// videoStage.style.opacity = showVideo ? 1 : 0;
+	// imageBG.style.opacity = showVideo ? 0 : 1;
+
 	contentHolder.style['margin-left'] = showVideo ? '15%' : '-200px';
 }
